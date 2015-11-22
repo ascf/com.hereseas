0d9d1259f17bad6affea9e0c08f7de33d68db956
hhz1992 <hhz1992@gmail.com>
2015-11-22 03:03:35 -0500

    test port

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ var config = {
     website_name: 'Hereseas',
     description: 'Make our life easier!',
     site_static_host: '',
-    port: 8080,
+    port: 80,
 
     auth_cookie_name: 'hereseas_cookie',
 
