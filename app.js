94877a3989865c8314666b7793b06e245b418222
hhz1992 <hhz1992@gmail.com>
2015-11-23 22:20:39 -0500

    test

--- app.js
+++ app.js
@@ -79,15 +79,23 @@ app.use(session({
 // app.use(allowCrossDomain);
 
 
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", req.headers.origin);
+//     res.header("Access-Control-Allow-Credentials", true);
+
+//     if ('OPTIONS' == req.method) {
+//         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+//         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization,X-Prototype-Version,Allow,*, Content-Length");
+//         res.send(200);
+//     }
+//     next();
+// });
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization,X-Prototype-Version,Allow,*, Content-Length");
     res.header("Access-Control-Allow-Credentials", true);
-
-    if ('OPTIONS' == req.method) {
-        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization,X-Prototype-Version,Allow,*, Content-Length");
-        res.send(200);
-    }
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     next();
 });
 
