36716a5c274c399963d26ec39f79fecd62004b57
hhz1992 <hhz1992@gmail.com>
2016-01-18 22:32:12 -0500

    email updated

--- common/email.js
+++ common/email.js
@@ -159,7 +159,7 @@ exports.sendEventEmail = function(email, name) {
             Body: { /* required */
                 Html: {
                     //Data: '<html><head></head><body><div><p>Hello world!</p></div></body></html>'
-                    Data: '<html><head><p>' + name + '</p></head><title>hereseas.com</title><body><div style="text-align:center"><img src="https://s3.amazonaws.com/hereseas-public-images/email/milktea.jpg"/></div></body></html>'
+                    Data: '<html><head><title>hereseas-event</title><style type="text/css">body {width: 860px;margin:0 auto;padding:0;background: white;}</style></head><body><p><strong>Welcome to Hereseas!</strong></p><p>Hereseas致力于打造北美校园生活线上社区，立足于留学生并服务于留学生。我们希望每一位用户可以在这里找到归属感，丰富留学生活。在这里，您可以寻找租房，匹配室友，还可以淘货，寻车，关注校内外活动，甚至评价教授！现在就开始吧！<br><a href="http://www.hereseas.com">www.hereseas.com</a></p><p><strong>最新活动</strong></p><p><strong>Right Here Night<br>“不一样的社交平台，不寻常的联谊方式，hereseas在2月13号之夜，集结华府各路精英，给你一个不一样的情人节联谊PA。”</strong></p><p><strong>活动详情<br>http://www.hereseas.com/#/event<br>购票链接<br><a href="https://hereseas.eventbrite.com">https://hereseas.eventbrite.com</a></strong></p><p><strong>活动优惠券<br>使用方法：在手机上展示此邮件，获得免费special drink一杯，每位会员只可领取一次</strong></p><p><img src="https://s3.amazonaws.com/hereseas-public-images/email/drink pic.jpg"/></p><p>hereseas.com<br>此处即海外, 他乡即故乡</p><p>关注我们的微信公号</p><p><img src="https://s3.amazonaws.com/hereseas-public-images/email/wechat pic.jpg"/></p><p>&nbsp;</p></body></html>'
                 }
             },
             Subject: { /* required */
